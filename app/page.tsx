e549ef2495df2858c051119018ddb153d39b6a65
jiyamathias <mathiasjiya2@gmail.com>
2026-05-09 18:30:27 +0100

    feat: image preview added

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <Hero />
       <HowItWorks />
       <Features />
-      <VideoDemo />
+      {/* <VideoDemo /> */}
       <AppPreview />
       <Spaces />
       <Waitlist />
